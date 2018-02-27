7f63067e37059afaaa99fb5f355e7f9e48e493ea
Ross Hill <rosshill88@gmail.com>
2018-02-27 19:49:26 +0000

    Create action working client side

--- client/src/app.js
+++ client/src/app.js
@@ -6,8 +6,15 @@ const request = new Request('http://localhost:3000/api/quotes');
 
 const appStart = function(){
   request.get(getQuotesRequestComplete);
+
+  const createQuoteButton = document.querySelector('#submit-quote');
+  createQuoteButton.addEventListener('click', createButtonClicked);
 }
 
+const createButtonClicked = function(event) {
+  event.preventDefault();
+  console.log('form submit clicked');
+}
 
 const getQuotesRequestComplete = function(allQuotes)  {
   allQuotes.forEach(function(quote) {
